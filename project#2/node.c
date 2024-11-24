f4d6fcab92b488a4d1d98582769a647a78f7e62b
kyoohyunlee <khyhlee@naver.com>
2024-11-24 19:39:09 +0900

    dev: fixing node.c

--- project#2/node.c
+++ project#2/node.c
@@ -2,214 +2,296 @@
  * Copyright(c) 2020-2024 All rights reserved by Heekuck Oh.
  * 이 프로그램은 한양대학교 ERICA 컴퓨터학부 학생을 위한 교육용으로 제작되었다.
  * 한양대학교 ERICA 학생이 아닌 자는 이 프로그램을 수정하거나 배포할 수 없다.
- * 프로그램을 수정할 경우 날짜, 학과, 학번, 이름, 수정 내용을 기록한다.
- *
- * 이규현 228
+ * 이규현 11/24 7:38
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "node.h"
 
-// 타입 정의
-typedef struct Feature Feature;
-typedef struct FeatureList FeatureList;
-typedef struct Class Class;
-typedef struct ClassList ClassList;
-typedef struct Expr Expr;
-typedef struct ExprList ExprList;
-typedef struct Case Case;
-typedef struct CaseList CaseList;
+/* Helper function to safely duplicate a string */
+char *strdup_safe(const char *src) {
+    if (!src) return NULL;
+    char *dup = malloc(strlen(src) + 1);
+    if (!dup) return NULL;
+    strcpy(dup, src);
+    return dup;
+}
 
-// 열거형 정의
-typedef enum {
-    EXPR_IF, EXPR_WHILE, EXPR_BLOCK, EXPR_INT, EXPR_STRING, EXPR_BOOL, EXPR_ID
-} ExprType;
-
-// 특성 정의
-struct Feature {
-    char *name;
-    char *type;
-    Expr *body;
-};
-
-struct FeatureList {
-    Feature *feature;
-    FeatureList *next;
-};
-
-// 클래스 정의
-struct Class {
-    char *type;
-    char *inherited;
-    FeatureList *features;
-};
-
-struct ClassList {
-    Class *class;
-    ClassList *next;
-};
-
-// 표현식 정의
-struct Expr {
-    ExprType type;
-    union {
-        struct {
-            Expr *condition;
-            Expr *then_branch;
-            Expr *else_branch;
-        } if_expr;
-        struct {
-            Expr *condition;
-            Expr *body;
-        } while_expr;
-        struct {
-            ExprList *block;
-        } block_expr;
-        int int_value;
-        char *string_value;
-        int bool_value;
-        char *id;
-    };
-};
-
-struct ExprList {
-    Expr *expr;
-    ExprList *next;
-};
-
-// Case 정의
-struct Case {
-    char *id;
-    char *type;
-    Expr *expr;
-};
-
-struct CaseList {
-    Case *case_expr;
-    CaseList *next;
-};
-
-// 함수 선언
-ClassList *create_class_list(Class *class);
-Class *create_class(char *type, char *inherited, FeatureList *features);
-FeatureList *create_feature_list(Feature *feature);
-Feature *create_feature(char *name, char *type, Expr *body);
-Expr *create_expr_int(int value);
-Expr *create_expr_string(char *value);
-ExprList *create_expr_list(Expr *expr);
-
-// 메모리 해제 함수
-void free_class_list(ClassList *list);
-void free_feature_list(FeatureList *list);
-void free_expr_list(ExprList *list);
-
-// 구현
-ClassList *create_class_list(Class *class) {
-    ClassList *list = malloc(sizeof(ClassList));
+/* Implementation of class list functions */
+class_list_t *create_class_list(class_t *class) {
+    class_list_t *list = malloc(sizeof(class_list_t));
     if (!list) return NULL;
     list->class = class;
     list->next = NULL;
     return list;
 }
 
-Class *create_class(char *type, char *inherited, FeatureList *features) {
-    Class *new_class = malloc(sizeof(Class));
+class_list_t *append_class_list(class_list_t *list, class_t *class) {
+    if (!list) return create_class_list(class);
+    class_list_t *current = list;
+    while (current->next) current = current->next;
+    current->next = create_class_list(class);
+    return list;
+}
+
+/* Implementation of class functions */
+class_t *create_class(char *type, char *inherited, feature_list_t *features) {
+    class_t *new_class = malloc(sizeof(class_t));
     if (!new_class) return NULL;
-    new_class->type = strdup(type);
-    new_class->inherited = inherited ? strdup(inherited) : NULL;
+    new_class->type = strdup_safe(type);
+    new_class->inherited = strdup_safe(inherited);
     new_class->features = features;
     return new_class;
 }
 
-FeatureList *create_feature_list(Feature *feature) {
-    FeatureList *list = malloc(sizeof(FeatureList));
+/* Implementation of feature list functions */
+feature_list_t *create_feature_list(feature_t *feature) {
+    feature_list_t *list = malloc(sizeof(feature_list_t));
     if (!list) return NULL;
     list->feature = feature;
     list->next = NULL;
     return list;
 }
 
-Feature *create_feature(char *name, char *type, Expr *body) {
-    Feature *feature = malloc(sizeof(Feature));
-    if (!feature) return NULL;
-    feature->name = strdup(name);
-    feature->type = strdup(type);
-    feature->body = body;
-    return feature;
+feature_list_t *append_feature_list(feature_list_t *list, feature_t *feature) {
+    if (!list) return create_feature_list(feature);
+    feature_list_t *current = list;
+    while (current->next) current = current->next;
+    current->next = create_feature_list(feature);
+    return list;
 }
 
-Expr *create_expr_int(int value) {
-    Expr *expr = malloc(sizeof(Expr));
-    if (!expr) return NULL;
-    expr->type = EXPR_INT;
-    expr->int_value = value;
-    return expr;
+/* Implementation of feature functions */
+feature_t *create_method(char *name, formal_list_t *formals, char *type, expr_t *body) {
+    feature_t *method = malloc(sizeof(feature_t));
+    if (!method) return NULL;
+    method->name = strdup_safe(name);
+    method->type = strdup_safe(type);
+    method->formals = formals;
+    method->body = body;
+    return method;
 }
 
-Expr *create_expr_string(char *value) {
-    Expr *expr = malloc(sizeof(Expr));
-    if (!expr) return NULL;
-    expr->type = EXPR_STRING;
-    expr->string_value = strdup(value);
-    return expr;
+feature_t *create_attribute(char *name, char *type, expr_t *init) {
+    feature_t *attribute = malloc(sizeof(feature_t));
+    if (!attribute) return NULL;
+    attribute->name = strdup_safe(name);
+    attribute->type = strdup_safe(type);
+    attribute->formals = NULL;  // Attributes do not have formals
+    attribute->body = init;
+    return attribute;
 }
 
-ExprList *create_expr_list(Expr *expr) {
-    ExprList *list = malloc(sizeof(ExprList));
+/* Implementation of formal list functions */
+formal_list_t *create_formal_list(formal_t *formal) {
+    formal_list_t *list = malloc(sizeof(formal_list_t));
     if (!list) return NULL;
-    list->expr = expr;
+    list->formal = formal;
     list->next = NULL;
     return list;
 }
 
-void free_class_list(ClassList *list) {
+formal_list_t *append_formal_list(formal_list_t *list, formal_t *formal) {
+    if (!list) return create_formal_list(formal);
+    formal_list_t *current = list;
+    while (current->next) current = current->next;
+    current->next = create_formal_list(formal);
+    return list;
+}
+
+formal_t *create_formal(char *name, char *type) {
+    formal_t *formal = malloc(sizeof(formal_t));
+    if (!formal) return NULL;
+    formal->name = strdup_safe(name);
+    formal->type = strdup_safe(type);
+    return formal;
+}
+
+/* Implementation of expression functions */
+expr_t *create_assign_expr(char *id, expr_t *expr) {
+    expr_t *assignment = malloc(sizeof(expr_t));
+    if (!assignment) return NULL;
+    assignment->type = ASSIGN_EXPR;
+    assignment->id = strdup_safe(id);
+    assignment->int_value = (intptr_t)expr;
+    return assignment;
+}
+
+expr_t *create_if_expr(expr_t *condition, expr_t *then_branch, expr_t *else_branch) {
+    expr_t *if_expr = malloc(sizeof(expr_t));
+    if (!if_expr) return NULL;
+    if_expr->type = IF_EXPR;
+    if_expr->if_expr.condition = condition;
+    if_expr->if_expr.then_branch = then_branch;
+    if_expr->if_expr.else_branch = else_branch;
+    return if_expr;
+}
+
+expr_t *create_while_expr(expr_t *condition, expr_t *body) {
+    expr_t *while_expr = malloc(sizeof(expr_t));
+    if (!while_expr) return NULL;
+    while_expr->type = WHILE_EXPR;
+    while_expr->while_expr.condition = condition;
+    while_expr->while_expr.body = body;
+    return while_expr;
+}
+
+expr_t *create_block_expr(expr_list_t *block) {
+    expr_t *block_expr = malloc(sizeof(expr_t));
+    if (!block_expr) return NULL;
+    block_expr->type = BLOCK_EXPR;
+    block_expr->block_expr.block_expr = block;
+    return block_expr;
+}
+
+expr_t *create_let_expr(char *id, char *type, expr_t *init, expr_t *body) {
+    expr_t *let_expr = malloc(sizeof(expr_t));
+    if (!let_expr) return NULL;
+    let_expr->type = BLOCK_EXPR;  // Reuse block_expr type for let
+    let_expr->id = strdup_safe(id);
+    let_expr->string_value = strdup_safe(type);
+    let_expr->int_value = (intptr_t)init;
+    let_expr->bool_value = (intptr_t)body;
+    return let_expr;
+}
+
+expr_t *create_case_expr(expr_t *expr, case_list_t *cases) {
+    expr_t *case_expr = malloc(sizeof(expr_t));
+    if (!case_expr) return NULL;
+    case_expr->type = CASE_EXPR;
+    case_expr->int_value = (intptr_t)expr;
+    case_expr->bool_value = (intptr_t)cases;
+    return case_expr;
+}
+
+expr_t *create_new_expr(char *type) {
+    expr_t *new_expr = malloc(sizeof(expr_t));
+    if (!new_expr) return NULL;
+    new_expr->type = NEW_EXPR;
+    new_expr->string_value = strdup_safe(type);
+    return new_expr;
+}
+
+expr_t *create_isvoid_expr(expr_t *expr) {
+    expr_t *isvoid_expr = malloc(sizeof(expr_t));
+    if (!isvoid_expr) return NULL;
+    isvoid_expr->type = ISVOID_EXPR;
+    isvoid_expr->int_value = (intptr_t)expr;
+    return isvoid_expr;
+}
+
+expr_t *create_not_expr(expr_t *expr) {
+    expr_t *not_expr = malloc(sizeof(expr_t));
+    if (!not_expr) return NULL;
+    not_expr->type = NOT_EXPR;
+    not_expr->int_value = (intptr_t)expr;
+    return not_expr;
+}
+
+expr_t *create_object_expr(char *id) {
+    expr_t *object_expr = malloc(sizeof(expr_t));
+    if (!object_expr) return NULL;
+    object_expr->type = OBJECT_EXPR;
+    object_expr->id = strdup_safe(id);
+    return object_expr;
+}
+
+expr_t *create_int_expr(int value) {
+    expr_t *int_expr = malloc(sizeof(expr_t));
+    if (!int_expr) return NULL;
+    int_expr->type = INT_EXPR;
+    int_expr->int_value = value;
+    return int_expr;
+}
+
+expr_t *create_string_expr(char *value) {
+    expr_t *string_expr = malloc(sizeof(expr_t));
+    if (!string_expr) return NULL;
+    string_expr->type = STRING_EXPR;
+    string_expr->string_value = strdup_safe(value);
+    return string_expr;
+}
+
+expr_t *create_bool_expr(bool value) {
+    expr_t *bool_expr = malloc(sizeof(expr_t));
+    if (!bool_expr) return NULL;
+    bool_expr->type = BOOL_EXPR;
+    bool_expr->bool_value = value;
+    return bool_expr;
+}
+
+/* Case list functions */
+case_list_t *append_case_list(case_list_t *list, case_t *new_case) {
+    if (!list) return create_case_list(new_case);
+    case_list_t *current = list;
+    while (current->next) current = current->next;
+    current->next = create_case_list(new_case);
+    return list;
+}
+
+case_list_t *create_case_list(case_t *new_case) {
+    case_list_t *list = malloc(sizeof(case_list_t));
+    if (!list) return NULL;
+    list->case_expr = new_case;
+    list->next = NULL;
+    return list;
+}
+
+case_t *create_case(char *id, char *type, expr_t *expr) {
+    case_t *case_expr = malloc(sizeof(case_t));
+    if (!case_expr) return NULL;
+    case_expr->id = strdup_safe(id);
+    case_expr->type = strdup_safe(type);
+    case_expr->expr = expr;
+    return case_expr;
+}
+
+/* Memory cleanup functions */
+void free_class_list(class_list_t *list) {
     while (list) {
+        class_list_t *next = list->next;
         free(list->class->type);
         if (list->class->inherited) free(list->class->inherited);
         free_feature_list(list->class->features);
         free(list->class);
-        ClassList *temp = list;
-        list = list->next;
-        free(temp);
+        free(list);
+        list = next;
     }
 }
 
-void free_feature_list(FeatureList *list) {
+void free_feature_list(feature_list_t *list) {
     while (list) {
+        feature_list_t *next = list->next;
         free(list->feature->name);
         free(list->feature->type);
+        free_expr_list(list->feature->body);
         free(list->feature);
-        FeatureList *temp = list;
-        list = list->next;
-        free(temp);
+        free(list);
+        list = next;
     }
 }
 
-void free_expr_list(ExprList *list) {
+void free_expr_list(expr_list_t *list) {
     while (list) {
-        if (list->expr->type == EXPR_STRING) free(list->expr->string_value);
+        expr_list_t *next = list->next;
+        if (list->expr->type == STRING_EXPR) free(list->expr->string_value);
         free(list->expr);
-        ExprList *temp = list;
-        list = list->next;
-        free(temp);
+        free(list);
+        list = next;
     }
 }
 
-// 메인 함수 예시
-int main() {
-    // 클래스 생성 예제
-    Feature *feature = create_feature("age", "int", create_expr_int(25));
-    FeatureList *feature_list = create_feature_list(feature);
-    Class *class = create_class("Person", NULL, feature_list);
-    ClassList *class_list = create_class_list(class);
-
-    // 출력
-    printf("Class: %s\n", class_list->class->type);
-    printf("Feature: %s, Type: %s\n", feature->name, feature->type);
-
-    // 메모리 해제
-    free_class_list(class_list);
-
-    return 0;
+void free_case_list(case_list_t *list) {
+    while (list) {
+        case_list_t *next = list->next;
+        free(list->case_expr->id);
+        free(list->case_expr->type);
+        free_expr_list((expr_list_t *)list->case_expr->expr);
+        free(list->case_expr);
+        free(list);
+        list = next;
+    }
 }
-
